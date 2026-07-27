0319cc6b2483b060232ab53bd40011a7c01ffc49
Grihika Jeloka <grihika@DESKTOP-6RGAMGV.localdomain>
2026-07-27 15:32:43 +0000

    BT-PAY-02: Allow editing Salary Detail amount after submission

--- buildtogether/hooks.py
+++ buildtogether/hooks.py
@@ -163,7 +163,7 @@ fixtures = [
     },
     {
         "dt": "Property Setter",
-        "filters": [["doc_type", "in", ["Employee", "Bank Account", "Payroll Entry"]]]
+        "filters": [["doc_type", "in", ["Employee", "Bank Account", "Payroll Entry", "Salary Detail"]]]
     },
     {
         "dt": "Web Form",
